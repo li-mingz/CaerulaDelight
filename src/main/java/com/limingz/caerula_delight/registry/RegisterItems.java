a80da7d7402854feecdcc9076e50ca8e7b8ee0d3
17330915383 <lm17330915383@163.com>
2026-01-16 23:12:04 +0800

    注册4排

--- src/main/java/com/limingz/caerula_delight/registry/RegisterItems.java
+++ src/main/java/com/limingz/caerula_delight/registry/RegisterItems.java
@@ -12,6 +12,7 @@ import java.util.LinkedHashSet;
 import java.util.function.Supplier;
 
 import static vectorwing.farmersdelight.common.registry.ModItems.drinkItem;
+import static vectorwing.farmersdelight.common.registry.ModItems.foodItem;
 
 public class RegisterItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CaerulaDelightMod.MODID);
@@ -22,6 +23,16 @@ public class RegisterItems {
         CREATIVE_TAB_ITEMS.add(block);
         return block;
     }
+
     public static final RegistryObject<Item> ROTTEN_FOAM = registerWithTab("rotten_foam",
             () -> new DrinkableItem(drinkItem().food(FoodValues.ROTTEN_FOAM), true, false));
+
+    public static final RegistryObject<Item> SEA_TERROR_CUTLET = registerWithTab("sea_terror_cutlet",
+            () -> new Item(foodItem(FoodValues.SEA_TERROR_CUTLET)));
+    public static final RegistryObject<Item> COOKED_SEA_TERROR_CUTLET = registerWithTab("cooked_sea_terror_cutlet",
+            () -> new Item(foodItem(FoodValues.COOKED_SEA_TERROR_CUTLET)));
+    public static final RegistryObject<Item> SEA_TERROR_CUTLET_STRIPS = registerWithTab("sea_terror_cutlet_strips",
+            () -> new Item(foodItem(FoodValues.SEA_TERROR_CUTLET_STRIPS)));
+    public static final RegistryObject<Item> COOKED_SEA_TERROR_CUTLET_STRIPS = registerWithTab("cooked_sea_terror_cutlet_strips",
+            () -> new Item(foodItem(FoodValues.COOKED_SEA_TERROR_CUTLET_STRIPS)));
 }
